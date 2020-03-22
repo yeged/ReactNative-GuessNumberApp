839087f533e389b6a2bace21722f58139002fa84
yeged <yigityceylan@gmail.com>
2020-03-22 21:21:19 +0300

    Adding Local Images

--- screens/GameOver.jsx
+++ screens/GameOver.jsx
@@ -1,13 +1,18 @@
 import React from "react";
-import {View, Text, StyleSheet, Button} from "react-native";
+import {View, Text, StyleSheet, Button, Image} from "react-native";
+import BodyText from "../components/BodyText";
+import TitleText from "../components/TitleText"
 
 
 function GameOver(props){
     return(
         <View style={styles.screen}>
-            <Text>Game is Over</Text>
-            <Text>Number of Rounds: {props.round}</Text>
-            <Text>Your Number : {props.userNumber}</Text>
+            <TitleText>Game is Over</TitleText>
+            <View style={styles.imageContainer}>
+            <Image style={styles.image} resizeMode="cover" source={require("../assets/original.png")}/>
+            </View>
+            <BodyText>Number of Rounds: {props.round}</BodyText>
+            <BodyText>Your Number : {props.userNumber}</BodyText>
             <Button title="NEW GAME" onPress={props.onNewGame} />
         </View>
     )
@@ -18,6 +23,20 @@ const styles = StyleSheet.create({
         flex:1,
         justifyContent: "center",
         alignItems: "center"
+    },
+    imageContainer:{
+        width: 250,
+        height: 250,
+        borderRadius: 125,
+        borderWidth:3,
+        borderColor: "black",
+        overflow: "hidden",
+        marginVertical:30
+    },
+    image:{
+        width: "100%",
+        height: "100%",
+        
     }
 })
 
