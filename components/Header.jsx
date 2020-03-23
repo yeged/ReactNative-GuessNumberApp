798d41b5a275a060968f9bf086affe80adab6efd
yeged <yigityceylan@gmail.com>
2020-03-23 20:55:03 +0300

    Platform Select and if Checks

--- components/Header.jsx
+++ components/Header.jsx
@@ -8,20 +8,25 @@ import Colors from "../constants/Colors"
 
 function Header(props) {
     return (
-        <View style={styles.header}>
+        <View style={{...styles.headerBase, ...Platform.select({ios: styles.headerIos, android: styles.headerAndroid})}}>
             <TitleText style={styles.headerTitle}>{props.title}</TitleText>
         </View>
     )
 }
 
 const styles = StyleSheet.create({
-    header: {
+    headerBase: {
         width: "100%",
         height: 90,
         alignItems: "center",
         justifyContent: "center",
-        backgroundColor: Platform.OS === "android" ? Colors.primary : Colors.accent,
         paddingTop: 36
+    },
+    headerIos:{
+        backgroundColor: Colors.accent
+    },
+    headerAndroid:{
+        backgroundColor: Colors.primary 
     },
     headerTitle: {
         color: "black",
--- components/MainButton.jsx
+++ components/MainButton.jsx
@@ -1,19 +1,32 @@
 import React from "react";
-import { View, Text, StyleSheet, TouchableOpacity } from "react-native";
+import { View, Text, StyleSheet, TouchableOpacity,TouchableNativeFeedback, Platform } from "react-native";
 
 import Colors from "../constants/Colors"
 
 function MainButton(props) {
+
+    let ButtonComponent = TouchableOpacity;
+    
+    if(Platform.OS === "android" && Platform.Version >= 21){
+        ButtonComponent = TouchableNativeFeedback
+    }
+
     return (
-        <TouchableOpacity activeOpacity={0.7} onPress={props.onPress}>
+        <View style={styles.buttonContainer}>
+        <ButtonComponent activeOpacity={0.7} onPress={props.onPress}>
             <View style={styles.button}>
                 <Text style={styles.buttonText}>{props.children}</Text>
             </View>
-        </TouchableOpacity>
+        </ButtonComponent>
+        </View>
     )
 }
 
 const styles = StyleSheet.create({
+    buttonContainer:{
+        borderRadius:25,
+        overflow: "hidden"
+    },
     button: {
         backgroundColor: Colors.primary,
         paddingVertical: 15,
