e1ec5c73856ca040f15a146e6e96d010515326b8
yeged <yigityceylan@gmail.com>
2020-03-23 23:03:21 +0300

    SafeAreaView

--- App.js
+++ App.js
@@ -1,5 +1,5 @@
 import React, { useState } from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, SafeAreaView} from 'react-native';
 import Header from "./components/Header"
 import GameScreen from "./screens/GameScreen"
 import GameScreen2 from "./screens/GameScreen2"
@@ -51,10 +51,10 @@ export default function App() {
   }
 
   return (
-    <View style={styles.screen}>
+    <SafeAreaView style={styles.screen}>
         <Header title="Guess A Number"/>
         {screenOutput}
-    </View>
+    </SafeAreaView>
   );
 }
 
--- components/MainButton.android.jsx
+++ components/MainButton.android.jsx
@@ -7,7 +7,7 @@ function MainButton(props) {
 
     let ButtonComponent = TouchableOpacity;
     
-    if(Platform.OS === "android" && Platform.Version >= 21){
+    if(Platform.Version >= 21){
         ButtonComponent = TouchableNativeFeedback
     }
 
